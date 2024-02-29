3cf35871c21aee6534f7f73dc586bce5da779cc9
Anbraten <6918444+anbraten@users.noreply.github.com>
2024-02-29 22:52:01 +0100

    remove old dashboard

--- packages/dashboard-old/lib/hooks/useFunctionsUsage.ts
+++
@@ -1,9 +0,0 @@
-import { trpc } from 'lib/trpc';
-
-const useFunctionsUsage = ({ functions }: { functions: string[] }) => {
-  return trpc.usage.useQuery({
-    functions,
-  });
-};
-
-export default useFunctionsUsage;
