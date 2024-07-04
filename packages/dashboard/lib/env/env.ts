5fdab119a2022cd78cecef00b8b333c3a3c8626d
Anbraten <6918444+anbraten@users.noreply.github.com>
2024-07-05 00:37:54 +0200

    adjust env

--- packages/dashboard/lib/env/env.ts
+++ packages/dashboard/lib/env/env.ts
@@ -4,4 +4,11 @@ declare global {
   }
 }
 
-export const getEnv = () => globalThis?.window?.env || process.env;
+export const getEnv = () => {
+  const env = globalThis?.window?.env || process.env;
+
+  return {
+    LAGOSS_ROOT_SCHEM: env.LAGOSS_ROOT_SCHEM,
+    LAGOSS_ROOT_DOMAIN: env.LAGOSS_ROOT_DOMAIN,
+  };
+};
