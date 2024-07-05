ffc6f5a00c2da896c3fd353015e51fa314218a26
Anbraten <6918444+anbraten@users.noreply.github.com>
2024-07-05 03:16:35 +0200

    enhance

--- crates/cli/bin/install.js
+++ crates/cli/bin/install.js
@@ -1,16 +1,17 @@
 import { spawnSync } from 'node:child_process';
 import fs from 'node:fs/promises';
 
-fs.stat('../bin/install.js')
-  .then(() => {
-    import('../bin/install.js');
+fs.stat('./dist/install.js')
+  .then(async () => {
+    await import('../dist/install.js');
   })
-  .catch(() => {
+  .catch(async err => {
+    console.error('Lagoss cli js files not found', err.message);
     console.log('Building the lagoss cli ...');
     const { status } = spawnSync('pnpm', ['build:js']);
     if (status !== 0) {
       throw new Error('Failed to build the lagoss cli');
     } else {
-      import('../bin/install.js');
+      await import('../dist/install.js');
     }
   });
