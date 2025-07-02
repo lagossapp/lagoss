5320e29f2edbbc93c898f39ccc38a1c4346f933d
Anbraten <anton@ju60.de>
2025-07-02 14:03:06 +0200

    rm ui

--- packages/ui/src/components/Copiable/Copiable.stories.tsx
+++
@@ -1,8 +0,0 @@
-import { ComponentStory, ComponentMeta } from '@storybook/react';
-import { Copiable } from '..';
-
-export default {
-  component: Copiable,
-} as ComponentMeta<typeof Copiable>;
-
-export const Default: ComponentStory<typeof Copiable> = () => <Copiable value="Value">Copy me</Copiable>;
