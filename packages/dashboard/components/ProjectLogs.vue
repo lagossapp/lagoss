2b6f370043ce8700f91f18b6cf8fac73edfd6ce9
Anbraten <anton@ju60.de>
2025-07-02 10:18:34 +0200

    enhance logs

--- packages/dashboard/components/ProjectLogs.vue
+++ packages/dashboard/components/ProjectLogs.vue
@@ -1,23 +1,47 @@
 <template>
   <div class="flex min-h-0 flex-col overflow-y-auto p-2" ref="logsView">
-    <UContainer
-      v-if="logs?.length === 0"
-      class="mt-4 w-full rounded border border-gray-200 py-16 text-center hover:shadow"
-    >
-      <p class="font-bold text-gray-500">No logs found for this project.</p>
-
-      <p class="text-gray-500">
-        You can use <span class="rounded-md bg-gray-200 px-1">console.log</span> in your code to log messages. Visit
-        <a :href="getFullCurrentDomain({ name: project.name })" target="_blank" class="text-blue-500 hover:underline"
-          >your project</a
-        >
-        to see the logs in action! 😉
-      </p>
-    </UContainer>
-    <div v-for="log in logs" :key="log.timestamp" class="flex gap-2 rounded-sm px-1 text-sm hover:bg-gray-200">
-      <span class="text-gray-500">{{ dayjs(log.timestamp).format('HH:mm:ss') }}</span>
-      <span class="">{{ log.message }}</span>
+    <div class="mb-4 flex justify-end gap-2">
+      <USelect v-model="level" :options="logLevels" class="w-48">
+        <!-- <template #leading>
+          <span>Level:</span>
+        </template> -->
+      </USelect>
+      <USelect v-model="timeframe" :options="timeFrames" class="w-48" />
     </div>
+
+    <Card>
+      <div v-if="logs?.length === 0">
+        <p class="font-bold text-gray-500">No logs found for this project.</p>
+
+        <p class="text-gray-500">
+          You can use <span class="rounded-md bg-gray-200 px-1">console.log('Hello world!')</span> in your code to log
+          messages.
+        </p>
+        <p class="text-gray-500">
+          Visit
+          <a :href="getFullCurrentDomain({ name: project.name })" target="_blank" class="text-blue-500 hover:underline"
+            >your project</a
+          >
+          to see the logs in action! 😉
+        </p>
+      </div>
+
+      <div class="flex flex-col gap-1">
+        <div
+          v-for="log in logs"
+          :key="log.timestamp"
+          class="flex items-center gap-2 rounded-md p-1 text-white"
+          :class="{
+            'bg-yellow-400': log.level === 'warn',
+            'bg-red-400': log.level === 'error',
+            'bg-blue-400': log.level === 'debug',
+          }"
+        >
+          <span class="text-xs">{{ dayjs(log.timestamp).format('DD.MM.YYYY HH:mm:ss') }}</span>
+          <span class="flex-1 text-sm">{{ log.message }}</span>
+        </div>
+      </div>
+    </Card>
   </div>
 </template>
 
@@ -30,14 +54,56 @@ const props = defineProps<{
   project: Project;
 }>();
 
+const level = ref('all');
+const timeframe = ref('Last hour');
+
+const logLevels = [
+  {
+    label: 'all',
+    value: 'all',
+  },
+  {
+    label: 'debug',
+    value: 'debug',
+  },
+  {
+    label: 'info',
+    value: 'info',
+  },
+  {
+    label: 'warn',
+    value: 'warn',
+  },
+  {
+    label: 'error',
+    value: 'error',
+  },
+];
+
+const timeFrames = [
+  {
+    label: 'Last hour',
+    value: 'Last hour',
+  },
+  {
+    label: 'Last 24 hours',
+    value: 'Last 24 hours',
+  },
+  {
+    label: 'Last week',
+    value: 'Last week',
+  },
+];
+
 // TODO: auto update logs
 // TODO: auto scroll to bottom
 const { data: logs, refresh: refreshLogs } = await useFetch(() => `/api/projects/${props.project.name}/logs`, {
-  query: {
-    level: 'all',
-    timeframe: 'Last hour',
-  },
+  query: computed(() => ({
+    level: level.value,
+    timeframe: timeframe.value,
+  })),
 });
+
 onMounted(() => {
   setInterval(() => {
     refreshLogs();
