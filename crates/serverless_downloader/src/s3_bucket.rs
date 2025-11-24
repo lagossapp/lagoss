cb962c677bedc0a7265eaca8de6ab8db8e65bfca
Anbraten <anton@ju60.de>
2025-11-24 19:54:58 +0100

    revert s3 update

--- crates/serverless_downloader/src/s3_bucket.rs
+++ crates/serverless_downloader/src/s3_bucket.rs
@@ -5,11 +5,11 @@ use s3::Bucket;
 use super::Downloader;
 
 pub struct S3BucketDownloader {
-    bucket: Box<Bucket>,
+    bucket: Bucket,
 }
 
 impl S3BucketDownloader {
-    pub fn new(bucket: Box<Bucket>) -> Self {
+    pub fn new(bucket: Bucket) -> Self {
         Self { bucket }
     }
 }
