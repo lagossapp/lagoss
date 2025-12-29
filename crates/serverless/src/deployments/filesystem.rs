2d761d6a949e1d13ab159c6700238164275a6323
Anbraten <anton@ju60.de>
2025-12-29 09:50:02 +0100

    fix last

--- crates/serverless/src/deployments/filesystem.rs
+++ crates/serverless/src/deployments/filesystem.rs
@@ -16,6 +16,7 @@ pub fn create_deployments_folder() -> Result<()> {
 }
 
 pub fn rm_deployment(deployment_id: &str) -> Result<()> {
+    // TODO: use some compiler flag instead of env var
     let skip = std::env::var("LAGOSS_TEST_SKIP_RM_DEPLOYMENT").is_ok();
     if !skip {
         let path = Path::new(DEPLOYMENTS_DIR).join(deployment_id);
