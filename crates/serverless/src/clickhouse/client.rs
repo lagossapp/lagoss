04959de9527eed9a5cb556f5c0328fbe2808416c
Anbraten <anton@ju60.de>
2026-02-03 13:23:56 +0100

    cleanup

--- crates/serverless/src/clickhouse/client.rs
+++ crates/serverless/src/clickhouse/client.rs
@@ -12,6 +12,7 @@ pub fn get_database_name() -> String {
 pub async fn create_client() -> Result<Client, anyhow::Error> {
     let url = read_env_var("CLICKHOUSE_URL");
     let user = read_env_var("CLICKHOUSE_USER");
+    let database = read_env_var("CLICKHOUSE_DATABASE");
 
     let mut client = Client::default().with_url(url).with_user(user);
 
@@ -19,11 +20,9 @@ pub async fn create_client() -> Result<Client, anyhow::Error> {
         client = client.with_password(password);
     }
 
-    let database = get_database_name();
-
     // Ensure the database exists
     client
-        .query(format!("CREATE DATABASE IF NOT EXISTS {}", get_database_name()).as_str())
+        .query(format!("CREATE DATABASE IF NOT EXISTS {}", database).as_str())
         .execute()
         .await?;
 
