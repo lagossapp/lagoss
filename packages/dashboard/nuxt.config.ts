a6dcb6427b5b42388c26365245314b61bc226620
Anbraten <anton@ju60.de>
2026-02-24 21:08:38 +0100

    fix

--- packages/dashboard/nuxt.config.ts
+++ packages/dashboard/nuxt.config.ts
@@ -1,7 +1,7 @@
 import { defineNuxtConfig } from 'nuxt/config';
 
 export default defineNuxtConfig({
-  modules: ['@nuxt/ui', '@nuxtjs/tailwindcss'],
+  modules: ['@nuxt/ui'],
 
   runtimeConfig: {
     auth: {
